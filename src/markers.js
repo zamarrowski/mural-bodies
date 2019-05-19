b43ef60d0d7f63b3ac8ce34d46766a49e17bd386
Sergio Zamarro <sergiozamarro@hotmail.com>
2019-05-19 18:42:25 +0200

    Changed markers and names

--- src/markers.js
+++ src/markers.js
@@ -3,7 +3,7 @@ import Leaflet from 'leaflet'
 export default [
   {
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
     name: 'Susana Davila',
@@ -13,64 +13,64 @@ export default [
   },
   {
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Javi',
+    name: 'Javier Garrido',
     position: [40.4074061,-3.7005607],
     mural: 'Zeta1970',
     image: '/images/javi.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Nunu',
+    name: 'Nuria Salas',
     position: [40.4030268,-3.69791],
     mural: 'Erb Mon',
     image: '/images/nunu.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Zam',
+    name: 'Sergio Zamarro',
     position: [40.5427613,-3.6361659],
     mural: 'Sabek',
     image: '/images/zam1.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Luso',
+    name: 'Luis Manuel Encinas',
     position: [40.542807, -3.636050],
     mural: 'Sabek',
     image: '/images/luso.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Suzanne',
+    name: 'Suzanne Bihina',
     position: [40.407790, -3.703411],
     mural: 'Pro176',
     image: '/images/suzanne.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Andrea',
+    name: 'Andrea Betetta',
     position: [40.407707, -3.703342],
     mural: 'Pro176',
     image: '/images/andrea.jpeg'
   },{
     icon: Leaflet.icon({
-      iconUrl: 'http://www.myiconfinder.com/uploads/iconsets/256-256-6096188ce806c80cf30dca727fe7c237.png',
+      iconUrl: '/images/icon.png',
       iconSize:     [25, 35],
     }),
-    name: 'Zam',
+    name: 'Sergio Zamarro',
     position: [40.406985,-3.7027574],
     mural: 'Diego Diego',
     image: '/images/zam2.jpeg'
